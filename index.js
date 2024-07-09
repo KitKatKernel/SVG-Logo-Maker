62eebaf31c5600391fdf7b55f58d4c3b1faa00ba
KitKatKernel <167586496+KitKatKernel@users.noreply.github.com>
2024-07-09 19:19:23 -0400

    Fixed the bug, it was ANSI characters from the colors npm package.  I removed the colors from the application, unfortunately I couldn't figure out to make strip-ansi work.  using require() returned a bug I'm not sure how to fix atm.

--- index.js
+++ index.js
@@ -2,11 +2,22 @@ const { Circle, Triangle, Square } = require('./lib/shapes');
 const userInput = require('./lib/input');
 const fs = require('fs');
 
+// Function to ensure hex colors are prefixed with #
+function formatHexColor(color) {
+    const hexRegex = /^#?[0-9A-Fa-f]{3}$|^#?[0-9A-Fa-f]{6}$/;
+    if (hexRegex.test(color) && !color.startsWith('#')) {
+        return `#${color}`;
+    }
+    return color;
+}
+
 async function generateLogo() {
     try {
+        // Get user inputs
         const answers = await userInput();
 
         let shape;
+        // Create shape from user input
         switch (answers.shape) {
             case 'Circle':
                 shape = new Circle();
@@ -22,10 +33,14 @@ async function generateLogo() {
                 return;
         }
 
-        shape.setColor(answers.shapePrompt === 'Choose from available colors' ? answers.shapeChoice : answers.shapeHex);
+        // Ensure hex colors are prefixed with #, hex answer won't work without # in the fill
+        const shapeColor = formatHexColor(answers.shapePrompt === 'Choose from available colors' ? answers.shapeChoice : answers.shapeHex);
+        const textColor = formatHexColor(answers.textPrompt === 'Choose from available colors' ? answers.textChoice : answers.textHex);
 
-        const textColor = answers.textPrompt === 'Choose from available colors' ? answers.textChoice : answers.textHex;
+        // Set shape color
+        shape.setColor(shapeColor);
         
+        // Generate SVG content
         const svgContent = `
             <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
                 ${shape.render()}
@@ -33,6 +48,7 @@ async function generateLogo() {
             </svg>
         `;
 
+        // Write SVG to file 'logo.svg'
         fs.writeFileSync('logo.svg', svgContent);
         console.log('Generated logo.svg');
     } catch (error) {
@@ -40,4 +56,5 @@ async function generateLogo() {
     }
 }
 
+// Run the function to generate the logo
 generateLogo();
